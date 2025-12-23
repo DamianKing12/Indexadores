ab45fef3d8a0d05612866b533705d2f21da3a658
deivijosetorresramirez24-bit <deivijosetorresramirez24@gmail.com>
2025-12-23 13:44:14 -0400

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
 rootProject.name = "CloudstreamPlugins"
-include("SeriesKaoProvider")
+include(":SeriesKaoProvider")
