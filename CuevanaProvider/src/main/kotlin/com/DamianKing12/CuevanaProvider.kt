84036c029e5d41e7befad6f2fd1a30141656d4c2
PC <damian.a.king.r.12@gmail.com>
2025-12-27 15:17:46 -0400

    Cambios en los archivos Provider.kt de Cuevana y SeriesKao

--- CuevanaProvider/src/main/kotlin/com/DamianKing12/CuevanaProvider.kt
+++ CuevanaProvider/src/main/kotlin/com/DamianKing12/CuevanaProvider.kt
@@ -1,71 +1,24 @@
 package com.DamianKing12
 
 import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.plugins.CloudstreamPlugin // Importante para la etiqueta
 import org.jsoup.nodes.Element
 
-@CloudstreamPlugin
 class CuevanaProvider : MainAPI() {
+    override var name = "Cuevana 3"
     override var mainUrl = "https://cue.cuevana3.nu"
-    override var name = "Cuevana 3 Indexador"
-    override val hasMainPage = true
     override var lang = "es"
-    override val hasQuickSearch = false
-    override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
+    override var supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
 
     override suspend fun search(query: String): List<SearchResponse> {
-        val document = app.get("$mainUrl/?s=$query").document
-        // Selector para los artículos de búsqueda en Cuevana
+        val url = "$mainUrl/?s=${query.replace(" ", "+")}"
+        val document = app.get(url).document
         return document.select("ul.results-post article, div.result-item").mapNotNull {
-            it.toSearchResult()
-        }
-    }
+            val title = it.selectFirst("h2, .title")?.text() ?: return@mapNotNull null
+            val href = fixUrl(it.selectFirst("a")?.attr("href") ?: return@mapNotNull null)
+            val posterUrl = fixUrl(it.selectFirst("img")?.attr("src") ?: "")
 
-    private fun Element.toSearchResult(): SearchResponse? {
-        val title = this.selectFirst("h2, .title")?.text() ?: return null
-        val href = fixUrl(this.selectFirst("a")?.attr("href") ?: return null)
-        val posterUrl = fixUrl(this.selectFirst("img")?.attr("src") ?: "")
-
-        return if (href.contains("/serie/")) {
-            newTvSeriesSearchResponse(title, href, TvType.TvSeries) {
-                this.posterUrl = posterUrl
-            }
-        } else {
             newMovieSearchResponse(title, href, TvType.Movie) {
                 this.posterUrl = posterUrl
-            }
-        }
-    }
-
-    override suspend fun load(url: String): LoadResponse? {
-        val document = app.get(url).document
-        val title = document.selectFirst("h1.Title")?.text() ?: ""
-        val poster = document.selectFirst(".Image img")?.attr("src")
-        val plot = document.selectFirst(".Description p")?.text()
-
-        val type = if (url.contains("/serie/")) TvType.TvSeries else TvType.Movie
-
-        return if (type == TvType.TvSeries) {
-            val episodes = document.select("ul.EpisodesList li").mapNotNull {
-                val href = it.selectFirst("a")?.attr("href") ?: return@mapNotNull null
-                val name = it.selectFirst(".name")?.text() ?: "Episodio"
-                val seasonNum = it.selectFirst(".season")?.text()?.toIntOrNull()
-                val episodeNum = it.selectFirst(".episode")?.text()?.toIntOrNull()
-
-                newEpisode(href) {
-                    this.name = name
-                    this.season = seasonNum
-                    this.episode = episodeNum
-                }
-            }
-            newTvSeriesLoadResponse(title, url, TvType.TvSeries, episodes) {
-                this.posterUrl = poster
-                this.plot = plot
-            }
-        } else {
-            newMovieLoadResponse(title, url, TvType.Movie, url) {
-                this.posterUrl = poster
-                this.plot = plot
             }
         }
     }
--- SeriesKaoProvider/src/main/kotlin/com/DamianKing12/SeriesKaoProvider.kt
+++ SeriesKaoProvider/src/main/kotlin/com/DamianKing12/SeriesKaoProvider.kt
@@ -1,60 +1,26 @@
 package com.DamianKing12
 
 import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.plugins.CloudstreamPlugin // <--- IMPORTANTE
-import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.cloudstream3.utils.loadExtractor
-import org.jsoup.nodes.Element
+import com.lagradost.cloudstream3.utils.*
 
-@CloudstreamPlugin // <--- ESTA ES LA ETIQUETA QUE FALTABA
 class SeriesKaoProvider : MainAPI() {
-    override var name = "Series Kao Indexador"
+    override var name = "SeriesKao"
     override var mainUrl = "https://serieskao.top"
-    override var supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
     override var lang = "es"
+    override var supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
 
     override suspend fun search(query: String): List<SearchResponse> {
         val url = "$mainUrl/?s=${query.replace(" ", "+")}"
         val document = app.get(url).document
-
         return document.select("div.result-item").mapNotNull {
             val titleElement = it.selectFirst("div.title a")
             val title = titleElement?.text() ?: return@mapNotNull null
             val href = titleElement.attr("href")
+            val poster = it.selectFirst("img")?.attr("src")
 
-            val img = it.selectFirst("img")
-            val poster = img?.attr("data-src")?.ifBlank { img.attr("src") }
-
-            newMovieSearchResponse(title, href, TvType.TvSeries) {
+            newMovieSearchResponse(title, href, TvType.Movie) {
                 this.posterUrl = poster
             }
         }
-    }
-
-    override suspend fun loadLinks(
-        data: String,
-        isCasting: Boolean,
-        subtitleCallback: (SubtitleFile) -> Unit,
-        callback: (ExtractorLink) -> Unit
-    ): Boolean {
-        val doc = app.get(data).document
-
-        doc.select("iframe, a[href*='embed'], li.dooplay_player_option").amap {
-            var iframeUrl = it.attr("src").ifBlank {
-                it.attr("data-url").ifBlank { it.attr("href") }
-            }
-
-            if (iframeUrl.isNotEmpty()) {
-                if (iframeUrl.startsWith("//")) {
-                    iframeUrl = "https:$iframeUrl"
-                }
-
-                if (iframeUrl.startsWith("http")) {
-                    loadExtractor(iframeUrl, data, subtitleCallback, callback)
-                }
-            }
-        }
-
-        return true
     }
 }
